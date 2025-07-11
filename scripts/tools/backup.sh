b11cd7bf845e3cb03ca0e664fb9b1cb29d739ca6
d3m0k1d <d3m0k1d1337@gmail.com>
2025-07-11 15:12:29 +0300

    first version of backup script

---
+++ scripts/tools/backup.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+    cat << 'EOF'
+    
+                               /$$             /$$           /$$                
+                              |__/            | $$          | $$                
+  /$$$$$$$  /$$$$$$$  /$$$$$$  /$$  /$$$$$$  /$$$$$$        | $$$$$$$  /$$   /$$
+ /$$_____/ /$$_____/ /$$__  $$| $$ /$$__  $$|_  $$_/        | $$__  $$| $$  | $$
+|  $$$$$$ | $$      | $$  \__/| $$| $$  \ $$  | $$          | $$  \ $$| $$  | $$
+ \____  $$| $$      | $$      | $$| $$  | $$  | $$ /$$      | $$  | $$| $$  | $$
+ /$$$$$$$/|  $$$$$$$| $$      | $$| $$$$$$$/  |  $$$$/      | $$$$$$$/|  $$$$$$$
+|_______/  \_______/|__/      |__/| $$____/    \___/        |_______/  \____  $$
+                                  | $$                                 /$$  | $$
+                                  | $$                                |  $$$$$$/
+                                  |__/                                 \______/ 
+       /$$  /$$$$$$                 /$$$$$$  /$$         /$$         /$$        
+      | $$ /$$__  $$               /$$$_  $$| $$       /$$$$        | $$        
+  /$$$$$$$|__/  \ $$ /$$$$$$/$$$$ | $$$$\ $$| $$   /$$|_  $$    /$$$$$$$        
+ /$$__  $$   /$$$$$/| $$_  $$_  $$| $$ $$ $$| $$  /$$/  | $$   /$$__  $$        
+| $$  | $$  |___  $$| $$ \ $$ \ $$| $$\ $$$$| $$$$$$/   | $$  | $$  | $$        
+| $$  | $$ /$$  \ $$| $$ | $$ | $$| $$ \ $$$| $$_  $$   | $$  | $$  | $$        
+|  $$$$$$$|  $$$$$$/| $$ | $$ | $$|  $$$$$$/| $$ \  $$ /$$$$$$|  $$$$$$$        
+ \_______/ \______/ |__/ |__/ |__/ \______/ |__/  \__/|______/ \_______/        
+                                                                                
+EOF
+
+mkdir -p /home/$USER/backup
+backup_name="backup_$(date +%Y-%m-%d-%H-%M-%S).tar.gz"
+
+while true; do
+    echo "Please enter the path to the directory you want to backup:"
+    read path
+    if [ -z "$path" ]; then
+        echo "Path cannot be empty"
+        echo "Please try again or use the ctrl+c to exit"
+    fi
+    if [ ! -d "$path" ]; then
+        echo "Path does not exist"
+        echo "Please try again or use the ctrl+c to exit"
+    fi
+    if [ -d "$path" ]; then
+        tar -czvf /home/$USER/backup/$backup_name $path
+        echo "Backup created at /home/$USER/backup/$backup_name"
+        break
+    fi
+done
